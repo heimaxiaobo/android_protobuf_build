96b102bee59007616a6739ac69196e077bf0c43b
chenjinbo <zpjinbo@163.com>
2024-07-18 14:36:39 +0800

    6666666

--- jni/Android.mk
+++ jni/Android.mk
@@ -4,38 +4,38 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := libprotobuf-lite
 
 LOCAL_SRC_FILES :=\
-	src/google/protobuf/any_lite.cc\
-	src/google/protobuf/arena.cc\
-	src/google/protobuf/arenastring.cc\
-	src/google/protobuf/arenaz_sampler.cc\
-	src/google/protobuf/extension_set.cc\
-	src/google/protobuf/generated_enum_util.cc\
-	src/google/protobuf/generated_message_tctable_lite.cc\
-	src/google/protobuf/generated_message_util.cc\
-	src/google/protobuf/implicit_weak_message.cc\
-	src/google/protobuf/inlined_string_field.cc\
-	src/google/protobuf/io/coded_stream.cc\
-	src/google/protobuf/io/io_win32.cc\
-	src/google/protobuf/io/strtod.cc\
-	src/google/protobuf/io/zero_copy_stream.cc\
-	src/google/protobuf/io/zero_copy_stream_impl.cc\
-	src/google/protobuf/io/zero_copy_stream_impl_lite.cc\
-	src/google/protobuf/map.cc\
-	src/google/protobuf/message_lite.cc\
-	src/google/protobuf/parse_context.cc\
-	src/google/protobuf/repeated_field.cc\
-	src/google/protobuf/repeated_ptr_field.cc\
-	src/google/protobuf/stubs/bytestream.cc\
-	src/google/protobuf/stubs/common.cc\
-	src/google/protobuf/stubs/int128.cc\
-	src/google/protobuf/stubs/status.cc\
-	src/google/protobuf/stubs/statusor.cc\
-	src/google/protobuf/stubs/stringpiece.cc\
-	src/google/protobuf/stubs/stringprintf.cc\
-	src/google/protobuf/stubs/structurally_valid.cc\
-	src/google/protobuf/stubs/strutil.cc\
-	src/google/protobuf/stubs/time.cc\
-	src/google/protobuf/wire_format_lite.cc\
+	src/google/protobuf/any_lite.cc \
+	src/google/protobuf/arena.cc \
+	src/google/protobuf/arenastring.cc \
+	src/google/protobuf/arenaz_sampler.cc \
+	src/google/protobuf/extension_set.cc \
+	src/google/protobuf/generated_enum_util.cc \
+	src/google/protobuf/generated_message_tctable_lite.cc \
+	src/google/protobuf/generated_message_util.cc \
+	src/google/protobuf/implicit_weak_message.cc \
+	src/google/protobuf/inlined_string_field.cc \
+	src/google/protobuf/io/coded_stream.cc \
+	src/google/protobuf/io/io_win32.cc \
+	src/google/protobuf/io/strtod.cc \
+	src/google/protobuf/io/zero_copy_stream.cc \
+	src/google/protobuf/io/zero_copy_stream_impl.cc \
+	src/google/protobuf/io/zero_copy_stream_impl_lite.cc \
+	src/google/protobuf/map.cc \
+	src/google/protobuf/message_lite.cc \
+	src/google/protobuf/parse_context.cc \
+	src/google/protobuf/repeated_field.cc \
+	src/google/protobuf/repeated_ptr_field.cc \
+	src/google/protobuf/stubs/bytestream.cc \
+	src/google/protobuf/stubs/common.cc \
+	src/google/protobuf/stubs/int128.cc \
+	src/google/protobuf/stubs/status.cc \
+	src/google/protobuf/stubs/statusor.cc \
+	src/google/protobuf/stubs/stringpiece.cc \
+	src/google/protobuf/stubs/stringprintf.cc \
+	src/google/protobuf/stubs/structurally_valid.cc \
+	src/google/protobuf/stubs/strutil.cc \
+	src/google/protobuf/stubs/time.cc \
+	src/google/protobuf/wire_format_lite.cc
 
 #LOCAL_CFLAGS := -D GOOGLE_PROTOBUF_NO_RTTI=1 -D HAVE_PTHREAD=1
 LOCAL_CFLAGS := -D HAVE_PTHREAD=1
